0807530abd8fdb52a9db9aba0ddf63b286c07023
tmddnxx <lsw6878@naver.com>
2024-08-09 12:05:39 +0900

    feat: header 액세스토큰검증 기능 추가

--- src/app/(DashboardLayout)/layout/header/Header.tsx
+++ src/app/(DashboardLayout)/layout/header/Header.tsx
@@ -5,16 +5,87 @@ import Link from 'next/link';
 // components
 import Profile from './Profile';
 import { IconBellRinging, IconMenu } from '@tabler/icons-react';
+import { signOut, useSession } from 'next-auth/react';
+import { jwtDecode } from 'jwt-decode';
 
 interface ItemType {
   toggleMobileSidebar:  (event: React.MouseEvent<HTMLElement>) => void;
 }
 
-const Header = ({toggleMobileSidebar}: ItemType) => {
+const Header = ({toggleMobileSidebar}: ItemType) => { 
 
   // const lgUp = useMediaQuery((theme) => theme.breakpoints.up('lg'));
   // const lgDown = useMediaQuery((theme) => theme.breakpoints.down('lg'));
 
+  // 액세스토큰 로컬 스토리지 저장 및 자동갱신 
+  const session = useSession();
+  
+  if(session.status === 'authenticated'){
+    const checkTokenExpiry = async () => {
+      const accessToken = session.data.user.accessToken;
+
+      // 액세스 토큰이 존재하면 로컬 스토리지에 저장
+      if (accessToken) {
+      localStorage.setItem('accessToken', accessToken);
+      }
+      
+      let LSAT = localStorage.getItem('accessToken');
+      if (LSAT) {
+          const decoded = jwtDecode(LSAT);
+          const at_ex = decoded.exp; // 액세스 토큰 만료 시간
+          const currentTime = Math.floor(Date.now() / 1000); // 현재 시간
+
+          // 액세스 토큰 만료 5분 전인지 확인
+          if ((at_ex - currentTime) <= 5 * 60) {
+              console.log("만료 5분 전");
+
+              try {
+                  const response = await fetch('/api/auth/token', {
+                      method: 'POST',
+                      headers: {'Content-Type': 'application/json'},
+                      body: JSON.stringify({ accessToken: LSAT }),
+                      credentials: 'include',
+                  });
+                  if(!response.ok){
+                      const errorData = await response.json();
+                      console.error(errorData);
+
+                      handleLogout();
+                      return;
+                  }
+                  const data = await response.json();
+
+                  if (data.accessToken) {
+                      localStorage.setItem('accessToken', data.accessToken);
+                      console.log('새 액세스 토큰:', data.accessToken);
+
+                      // 새 액세스 토큰으로 갱신된 시간 확인
+                      LSAT = data.accessToken; // 갱신된 액세스 토큰으로 업데이트
+                      
+                      session.update({accessToken: data.accessToken});
+                      
+                  } else {
+                      console.log("새로운 토큰을 발급 받을 수 없습니다.");
+                  }
+              } catch (error) {
+                  console.error('토큰 갱신 오류:', error);
+                  handleLogout();
+              };
+            };
+          };
+    };
+
+    checkTokenExpiry();
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken');
+    
+    fetch('/api/auth/logout',{
+        method:'POST',
+    })
+    signOut();
+  };
 
   const AppBarStyled = styled(AppBar)(({ theme }) => ({
     boxShadow: 'none',
@@ -64,6 +135,9 @@ const Header = ({toggleMobileSidebar}: ItemType) => {
         <Stack spacing={1} direction="row" alignItems="center">
           <Button variant="contained" component={Link} href="/authentication/login"   disableElevation color="primary" >
             Login
+          </Button>
+          <Button variant="contained" onClick={handleLogout} disableElevation color="primary" >
+            LogOut
           </Button>
           <Profile />
         </Stack>
