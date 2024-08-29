f663088ab685a7068578f7dda201bd0be95569fe
portLee <hungu32@gmail.com>
2024-08-29 18:42:33 +0900

    fix: 리뷰 조회시 세션의 로그인 쿠키값 사용하도록 수정

--- src/app/api/reviews/route.ts
+++ src/app/api/reviews/route.ts
@@ -1,15 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { cookies } from "next/headers";
+import { getIronSession } from "iron-session";
+import { sessionOptions } from "@/utils/session";
+import { SessionData } from "@/utils/session";
 import RSAEncryptionService from '@/services/RSAEncryptionService';
 import BaeminApiService from '@/services/BaeminApiService';
 
-const LOGIN_INIT_URL = 'https://biz-member.baemin.com/v1/login/init';
-const LOGIN_URL = 'https://biz-member.baemin.com/v1/login';
-const PROFILE_URL = 'https://self-api.baemin.com/v1/session/profile';
-const SHOP_URL = 'https://self-api.baemin.com/v4/store/shops/search';
-const REVIEWS_URL = 'https://self-api.baemin.com/v1/review/shops';
-const USER_ID = '아이디';
-const PASSWORD = '비밀번호';
+const USER_ID = process.env.USER_ID;
+const PASSWORD = process.env.PASSWORD;
 const HARDCODED_PW = 'shd29pv5qp59shpjw4q2uoz7lb0l8xnrrjn2eikx7rsawq5wdmuv3im703yl';
+
+const getFormattedDate = (today: Date) => {
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    
+    return `${year}-${month}-${day}`;
+}
 
 // 배민 review 조회
 export async function GET(req: NextRequest) {
@@ -17,47 +24,69 @@ export async function GET(req: NextRequest) {
     const baeminApiService = BaeminApiService.getInstance();
 
     try {
-        // 공개키 조회
-        const { tag, value } = await baeminApiService.getPublic(LOGIN_INIT_URL);
-        console.log('Public Key (n):', tag);
-        console.log('Public Exponent (e):', value);
+        // // 공개키 조회
+        // const { tag, value } = await baeminApiService.getPublic();
+        // console.log('Public Key (n):', tag);
+        // console.log('Public Exponent (e):', value);
 
-        // 아이디 비밀번호 암호화
-        rsaEncryptionService.setPublic(tag, value);
-        const value1 = rsaEncryptionService.encrypt(USER_ID);
-        const value2 = rsaEncryptionService.encrypt(PASSWORD);
+        // // 아이디 비밀번호 암호화
+        // rsaEncryptionService.setPublic(tag, value);
+        // const value1 = rsaEncryptionService.encrypt(USER_ID);
+        // const value2 = rsaEncryptionService.encrypt(PASSWORD);
 
-        console.log('Encrypted value1:', value1);
-        console.log('Encrypted value2:', value2);
+        // console.log('Encrypted value1:', value1);
+        // console.log('Encrypted value2:', value2);
 
-        const payload = {
-            id: USER_ID,
-            pw: HARDCODED_PW,
-            token: "",
-            value1: value1,
-            value2: value2
+        // const payload = {
+        //     id: USER_ID,
+        //     pw: HARDCODED_PW,
+        //     token: "",
+        //     value1: value1,
+        //     value2: value2
+        // }
+
+        // console.log('Request payload:', payload);
+
+        // // 배민 로그인
+        // await baeminApiService.login(payload);
+
+        // // 사장님 고유 번호 조회
+        // const shopOwnerNumber = await baeminApiService.getProfile();
+        // console.log(`shopOwnerNumber: ${shopOwnerNumber}`);
+
+        // const data = await baeminApiService.getShopInfo();
+        
+        // // 필요한 정보만 추출
+        // const shops = data.content.map(({ shopNo, name, serviceType }) => ({
+        //     shopNo,
+        //     name,
+        //     serviceType
+        // }));
+        // console.log(shops);
+
+        const session = await getIronSession<SessionData>(cookies(), sessionOptions);
+
+        console.log("session: " + JSON.stringify(session));
+        console.log("cookie: " + session.cookie);
+
+        const today = new Date();
+        const monthAgo = new Date(today);
+        monthAgo.setDate(today.getDate() - 26);
+        monthAgo.setMonth(today.getMonth() - 5);
+        const to = getFormattedDate(today);
+        const from = getFormattedDate(monthAgo);
+
+        console.log('from:', from);
+        console.log('to:', to);
+
+        const params = {
+            from: from,
+            to: to,
+            offset: 0,
+            limit: 10
         }
 
-        console.log('Request payload:', payload);
-
-        // 배민 로그인
-        await baeminApiService.login(LOGIN_URL, payload);
-
-        // 사장님 고유 번호 조회
-        const shopOwnerNumber = await baeminApiService.getProfile(PROFILE_URL);
-        console.log(`shopOwnerNumber: ${shopOwnerNumber}`);
-
-        const data = await baeminApiService.getShopInfo(`${SHOP_URL}?shopOwnerNo=${shopOwnerNumber}`);
-        
-        // 필요한 정보만 추출
-        const shops = data.content.map(({ shopNo, name, serviceType }) => ({
-            shopNo,
-            name,
-            serviceType
-        }));
-        console.log(shops);
-
-        const reviewData = await baeminApiService.getReviews(`${REVIEWS_URL}/${shops[0].shopNo}/reviews?from=2024-02-10&to=2024-08-07&offset=0&limit=10`);
+        const reviewData = await baeminApiService.getReviews(params, session.cookie);
 
         return NextResponse.json(reviewData.reviews);
     } catch (error) {
