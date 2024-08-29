23fd7cec022a0cca6a38d278e30836b3c21ae3db
portLee <hungu32@gmail.com>
2024-08-29 18:45:56 +0900

    fix: 배민API URL 환경변수에서 가져오도록 수정

--- src/services/BaeminApiService.ts
+++ src/services/BaeminApiService.ts
@@ -9,8 +9,15 @@ interface PublicKeyResponse {
 
 const BASE_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
-    'Referer': 'https://biz-member.baemin.com/login?returnUrl=https%3A%2F%2Fself.baemin.com%2Fshops%2Freviews'
+    "Origin": "https://self.baemin.com",
+    'Referer': 'https://self.baemin.com/'
 };
+
+const LOGIN_URL = process.env.BIZ_API_BASE_URL + '/v1/login';
+const LOGIN_INIT_URL = LOGIN_URL + '/init';
+const PROFILE_URL = process.env.SELF_API_BASE_URL + '/v1/session/profile';
+const SHOP_URL = process.env.SELF_API_BASE_URL + '/v4/store/shops/search';
+const REVIEWS_BASE_URL = process.env.SELF_API_BASE_URL + '/v1/review/shops';
 
 const cookieJar = new CookieJar();
 const fetchWithCookies = makeFetchCookie(fetch, cookieJar);
@@ -27,13 +34,13 @@ export default class BaeminApiService {
     }
 
     // 공개키 조회
-    async getPublic(url: string): Promise<PublicKeyResponse> {
+    public async getPublic(): Promise<PublicKeyResponse> {
         const options = {
             headers: BASE_HEADERS,
             cache: 'no-cache' as RequestCache
         };
 
-        const response = await fetchWithCookies(url, options);
+        const response = await fetchWithCookies(LOGIN_INIT_URL, options);
         if (!response.ok) {
             throw new Error(`Failed to fetch public key: ${response.statusText}`);
         }
@@ -47,7 +54,7 @@ export default class BaeminApiService {
     }
 
     // 로그인
-    async login(url: string, credentials: any) {
+    public async login(credentials: any): Promise<string> {
         const headers = new Headers({
             ...BASE_HEADERS,
             "Content-Type": "application/json"
@@ -59,7 +66,7 @@ export default class BaeminApiService {
             body: JSON.stringify(credentials)
         };
 
-        const response = await fetchWithCookies(url, options);
+        const response = await fetchWithCookies(LOGIN_URL, options);
 
         if (!response.ok) {
             throw new Error(`Beamin Login failed: ${response.statusText}`);
@@ -68,10 +75,14 @@ export default class BaeminApiService {
         const responseBody = await response.text();
         console.log('Response status:', response.status);
         console.log('Response body:', responseBody);
+
+        const cookies = response.headers.get('set-cookie') || "";
+
+        return cookies;
     }
 
     // 프로필 조회
-    async getProfile(url: string): Promise<string>{
+    public async getProfile(): Promise<string>{
         const headers = new Headers({
             ...BASE_HEADERS,
             "Service-Channel": "SELF_SERVICE_PC"
@@ -81,7 +92,7 @@ export default class BaeminApiService {
             headers: headers
         };
 
-        const response = await fetchWithCookies(url, options);
+        const response = await fetchWithCookies(PROFILE_URL, options);
         if (!response.ok) {
             throw new Error(`Failed to fetch profile: ${response.statusText}`);
         }
@@ -94,7 +105,7 @@ export default class BaeminApiService {
     }
 
     // 가게 정보 조회
-    async getShopInfo(url: string): Promise<any>{
+    public async getShopInfo(): Promise<any>{
         const headers = new Headers({
             ...BASE_HEADERS,
             "Service-Channel": "SELF_SERVICE_PC"
@@ -104,7 +115,7 @@ export default class BaeminApiService {
             headers: headers
         };
 
-        const response = await fetchWithCookies(url, options);
+        const response = await fetchWithCookies(SHOP_URL, options);
         if (!response.ok) {
             throw new Error(`Failed to fetch shop info: ${response.statusText}`);
         }
@@ -116,17 +127,20 @@ export default class BaeminApiService {
     }
 
     // 리뷰 목록 조회
-    async getReviews(url: string): Promise<any>{
+    public async getReviews(params: any, cookie: string): Promise<any>{
+        console.log("getReviews cookie: " + cookie);
         const headers = new Headers({
             ...BASE_HEADERS,
+            "Cookie": cookie,
             "Service-Channel": "SELF_SERVICE_PC"
         });
 
         const options = {
-            headers: headers
+            headers: headers,
         };
 
-        const response = await fetchWithCookies(url, options);
+        const queryString = new URLSearchParams(params).toString();
+        const response = await fetchWithCookies(REVIEWS_BASE_URL + `/14104060/reviews?${queryString}`, options);
         if (!response.ok) {
             throw new Error(`Failed to fetch reviews: ${response.statusText}`);
         }
@@ -137,5 +151,33 @@ export default class BaeminApiService {
         return responseData;
     }
 
-    // 답글 등록 
+    // 리뷰 답글 등록 
+    public async addComment(comments: any, cookie: string) {
+
+        const headers = new Headers({
+            ...BASE_HEADERS,
+            "Content-Type": "application/json;charset=UTF-8",
+            "Cookie": cookie,
+            "Service-Channel": "SELF_SERVICE_PC"
+        });
+
+        const options = {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(comments),
+            cookie: cookie
+        };
+
+        console.log(comments);
+
+        const response = await fetchWithCookies(REVIEWS_BASE_URL + '/14104060/reviews/comments', options);
+
+        if (!response.ok) {
+            throw new Error(`Add comment failed: ${response.statusText}`);
+        }
+
+        const responseBody = await response.text();
+        console.log('Response status:', response.status);
+        console.log('Response body:', responseBody);
+    }
 }
