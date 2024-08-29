ec3ac346315abe3993aebf04e70d30718a492d0c
portLee <hungu32@gmail.com>
2024-08-29 18:54:32 +0900

    fix: 불필요한 컴포넌트 삭제

--- src/app/(DashboardLayout)/reviews/page.tsx
+++ src/app/(DashboardLayout)/reviews/page.tsx
@@ -1,7 +1,5 @@
 'use client';
-import { Typography } from '@mui/material';
 import PageContainer from '@/app/(DashboardLayout)/components/container/PageContainer';
-import DashboardCard from '@/app/(DashboardLayout)/components/shared/DashboardCard';
 import ProductPerformance from '../components/dashboard/ProductPerformance';
 
 
